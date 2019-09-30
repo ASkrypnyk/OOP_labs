dc693b6d60298b32eb2193e7e0c119c786114d1c
ASkrypnyk <47245041+ASkrypnyk@users.noreply.github.com>
2019-10-01 00:12:43 +0300

    Add files via upload

--- Lab1/DoubleLinkedList.cpp
+++ Lab1/DoubleLinkedList.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 template<typename T>
+DoubleLinkedList<T>:: ~DoubleLinkedList() {
+	Node<T>* temp;
+	while (head) {
+		temp = head;
+		head = head->next;
+		delete temp;
+	}
+}
+
+template<typename T>
 void DoubleLinkedList<T>::add(T data) {
 	Node<T>* curr = new Node;
 	curr->data = data;
@@ -17,15 +27,16 @@ void DoubleLinkedList<T>::add(T data) {
 		curr->prev = tail;
 		curr->next = nullptr;
 		tail->next = curr;
-		tail = temp;
+		tail = curr;
 	}
 
 }
 
 template<typename T>
-
-
-
-
-
-template<typename T>
+void DoubleLinkedList<T>::print() {
+	Node<T>* curr = new Node;
+	while (curr != nullptr) {
+		cout << curr->data;
+		curr = curr->next;
+	}
+}
